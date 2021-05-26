391f7e79f01f51e63ee64c0f2ae95545cbe15a94
Sachin Sharma <52964301+Maverick-01@users.noreply.github.com>
2021-05-26 14:13:34 +0530

    Update SetAa.c

--- Tree/SetAa.c
+++ Tree/SetAa.c
@@ -1,1 +1,83 @@
 #include <stdio.h>
+
+#include <stdlib.h>
+
+#include "btree.h"
+
+int main() {
+
+  int choice,n;
+
+  while(1) {
+
+    printf("\n1.Insert\n2.InOrder Traversal\n3.PreOrder Traversal\n4.PostOrder Traversal\n5.Search\n6.exit\n");
+
+    scanf("%d", &choice);
+
+    switch(choice) {
+
+      case 1:
+
+      insert();
+
+      break;
+
+      case 2:
+
+      inOrder (root);
+
+      break;
+
+      case 3:
+
+      preOrder (root);
+
+      break;
+
+      case 4:
+
+      postOrder (root);
+
+      break;
+
+      case 5:
+
+      if(root == NULL)
+
+      printf("\n Tree is empty \n");
+
+      else
+
+      {
+
+        printf("\n Enter data to search \n");
+
+        scanf("%d", &n);
+
+        if(searching(root, n))
+
+        printf("\n Data is not present in tree\n");
+
+        else
+
+        printf("\n Data is present\n");
+
+      }
+
+      break;
+
+      case 6:
+
+      exit(0);
+
+      default:
+
+      printf("\n Invalid Location n");
+
+      break;
+
+    }
+
+  }
+
+}
